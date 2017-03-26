149b8b526eef3142c00a8cbf6d8ebc3749f59550
iEnder <pkmn2024@gmail.com>
2017-03-25 23:28:22 -0400

    added image carousel

---
+++ src/components/Pages/Gallery/Carousel.vue
@@ -0,0 +1,17 @@
+<template>
+<div class='carousel' >
+    <span></span>
+    <div></div>
+    <span></span>
+</div>
+</template>
+ 
+<script>
+export default {
+name: 'Carousel'
+};
+</script>
+ 
+<style lang='scss' scoped>
+@import '../assets/_scss/_carousel';
+</style>
